e8a49fc9150432b214a0cc3ab09abee18bb7447e
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-01-31 22:45:28 +0300

    программа работает, но как ещё разбираюсь

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Num = {num}");
+int num1 = num / 100;
+Console.WriteLine($"Num1 = {num1}");
+int num2 = num % 100;
+Console.WriteLine($"Num2 = {num2}");
+int max = num2;
+if (num1 > max)
+{
+  max = num1;
+}
+Console.WriteLine($"Max = {max}");
