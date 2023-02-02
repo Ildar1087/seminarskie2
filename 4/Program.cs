e79ed1b54c5c4c72c2ba8742b4a7b92734ba622d
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-02 16:10:48 +0300

    закоммитил изм в 4 папке

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+int n1 =7, n2 = 23;
+if (x % n1 == 0 && x % n2 == 0) 
+Console.WriteLine("да, кратно");
+else
+Console.WriteLine("не кратно");
+  //Console.WriteLine($"не кратно остаток - {x % y}");
