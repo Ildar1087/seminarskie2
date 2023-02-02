e40090ab8010db48ba5d657733d894330922ff7b
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-02 16:12:26 +0300

    Рабочий код

---
+++ dz№15/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.Write("введите цифру дня недели: ");
+int numbDayWeek = int.Parse(Console.ReadLine());
+if (numbDayWeek < 1 || numbDayWeek > 7)
+{
+  Console.WriteLine("не то ввели, аккуратней");
+}
+if (numbDayWeek == 1)
+{
+  Console.WriteLine("нет, понедельник рабочий день");
+}
+if (numbDayWeek == 2)
+{
+  Console.WriteLine("нет, вторник рабочий день");
+}
+if (numbDayWeek == 3)
+{
+  Console.WriteLine("нет, среда рабочий день");
+}
+if (numbDayWeek == 4)
+{
+  Console.WriteLine("нет, четверг рабочий день");
+}
+if (numbDayWeek == 5)
+{
+  Console.WriteLine("нет, пятница рабочий день");
+}
+if (numbDayWeek == 6)
+{
+  Console.WriteLine("Да - Суббота выходной");
+}
+if (numbDayWeek == 7)
+{
+  Console.WriteLine("Да - Воскресенье выходной");
+}
