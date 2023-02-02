1966e8fb12c747ba716d3d1be9868f5249b3351c
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-02 10:07:17 +0300

    работает программа правильно

---
+++ dz№10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"num = {num}");
+int result = (num / 10);
+int res2 = result % 10;
+Console.Write($"{num} -> {res2 % 10}");
