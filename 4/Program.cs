213b22fd4a5919dc579c7f4c8f07e4d4222c02ec
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-03 12:51:00 +0300

    исправлены недочеты

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.Clear();
 Console.Write("Введите первое число: ");
 int x = int.Parse(Console.ReadLine());
-int n1 =7, n2 = 23;
-if (x % n1 == 0 && x % n2 == 0) 
+if (x % 7 == 0 && x % 23 == 0) 
 Console.WriteLine("да, кратно");
 else
 Console.WriteLine("не кратно");
